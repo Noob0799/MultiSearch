9e5a8261b452fcd45fc062b3a6f6a42989ad4d4b
Noob0799 <uchihaitachi0799@gmail.com>
2024-10-21 00:33:15 +0530

    Code changes add keyboard navigation to search results

--- src/components/multi-search.js
+++ src/components/multi-search.js
@@ -5,8 +5,10 @@ const MultiSearch = () => {
   const [inputValue, setInputValue] = useState("");
   const [autoSuggestions, setAutoSuggestions] = useState([]);
   const [isAutoSuggestOn, setAutoSuggestOn] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [selectedSuggestions, setSelectedSuggestions] = useState([]);
   const [selectedUsers, setSelectedUsers] = useState(new Set());
+  const [focusedIndex, setFocusedIndex] = useState(0);
   const elementRef = useRef();
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
@@ -29,26 +31,53 @@ const MultiSearch = () => {
     setSelectedSuggestions(tempSuggestions);
   };
   const fetchSuggestions = () => {
+    setIsLoading(true);
     fetch(`https://dummyjson.com/users/search?q=${inputValue}`)
       .then((res) => res.json())
       .then((res) => {
         let suggestions = [...res.users];
-        suggestions = suggestions.filter(suggestion => !selectedUsers.has(suggestion.email));
+        suggestions = suggestions.filter(
+          (suggestion) => !selectedUsers.has(suggestion.email)
+        );
         setAutoSuggestions(suggestions);
+        setIsLoading(false);
       });
   };
   const handleKeyDown = (event) => {
     const { key } = event;
     if (key == "Backspace") {
       const tempSelectedSuggestions = [...selectedSuggestions];
-      if (tempSelectedSuggestions.length > 0) {
+      if (!inputValue.length && tempSelectedSuggestions.length > 0) {
         const oSuggestion =
           tempSelectedSuggestions[tempSelectedSuggestions.length - 1];
         removeItem(oSuggestion.id, oSuggestion.email);
       }
     }
   };
+  const handleSuggestionsKeyDown = (event) => {
+    const { key } = event;
+    console.log(key);
+    switch (key) {
+      case "ArrowUp":
+        if (autoSuggestions.length)
+          setFocusedIndex((prevIndex) =>
+            prevIndex == 0 ? autoSuggestions.length - 1 : prevIndex - 1
+          );
+        break;
+      case "ArrowDown":
+        if (autoSuggestions.length)
+          setFocusedIndex((prevIndex) =>
+            prevIndex == autoSuggestions.length - 1 ? 0 : prevIndex + 1
+          );
+        break;
+      case "Enter":
+        if (autoSuggestions.length)
+          handleSuggestionClick(autoSuggestions[focusedIndex]);
+        break;
+    }
+  };
   useEffect(() => {
+    setFocusedIndex(0);
     if (isAutoSuggestOn && inputValue.length > 0) {
       fetchSuggestions();
     } else {
@@ -56,8 +85,19 @@ const MultiSearch = () => {
       setAutoSuggestions([]);
     }
   }, [inputValue.length]);
+  useEffect(() => {
+    const parentRect = document
+      .querySelector(".auto-suggestions-container")
+      .getBoundingClientRect();
+    const elem = document.querySelector(`.option-${focusedIndex}`);
+    if (elem) {
+      const rect = elem.getBoundingClientRect();
+      if (rect.top < parentRect.top || rect.bottom > parentRect.bottom)
+        elem.scrollIntoView();
+    }
+  }, [focusedIndex]);
   return (
-    <div className="main-container">
+    <div className="main-container" onKeyDown={handleSuggestionsKeyDown}>
       <div className="multi-search-container">
         <SelectedSuggestions
           selectedSuggestions={selectedSuggestions}
@@ -72,12 +112,18 @@ const MultiSearch = () => {
           ref={elementRef}
         />
       </div>
-      {autoSuggestions.length ? (
+      {isLoading ? (
+        <div className="auto-suggestions-container">
+          <div className="auto-suggestions-item">Loading...</div>
+        </div>
+      ) : (
         <div className="auto-suggestions-container">
           {autoSuggestions.map((suggestion, idx) => (
             <div
               key={suggestion.id}
-              className={`auto-suggestions-item`}
+              className={`auto-suggestions-item option-${idx} ${
+                focusedIndex == idx ? "selected-item" : ""
+              }`}
               onClick={() => handleSuggestionClick(suggestion)}
             >
               <img src={suggestion.image} alt={suggestion.firstName} />{" "}
@@ -85,8 +131,6 @@ const MultiSearch = () => {
             </div>
           ))}
         </div>
-      ) : (
-        <></>
       )}
     </div>
   );
